f09e85ff64e656f0badd3ad57c742f336cca6284
Matheus Vieira Ferreira <matheusvferreira17@gmail.com>
2022-12-12 11:20:21 -0300

    A refactoring has been done and added new importations

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Home } from './pages/Home'
-
-
-
 import "./pages/styles/global.css"
 
+import { Home } from './pages/Home'
+import "./pages/Home/home.css"
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Home/>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
