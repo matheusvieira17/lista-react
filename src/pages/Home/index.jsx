093412215a0bad2524a6753a863307cdd627c26b
Matheus Vieira Ferreira <matheusvferreira17@gmail.com>
2022-12-12 11:22:21 -0300

    New elements has been added

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,9 @@
 export const Home = () => {
   return (
-    <>
+    <div className="container">
       <h1> Lista de presença </h1>
       <input type="text" placeholder="Digite seu nome" />
       <button> Adicionar </button>
-    </>
+    </div>
   )
 }
