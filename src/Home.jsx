d2b254c595b6a34ddb0621e6ab19fbbc60bac906
Matheus Vieira Ferreira <matheusvferreira17@gmail.com>
2022-12-12 10:19:46 -0300

    Creating new folder

--- src/Home.jsx
+++
@@ -1,7 +0,0 @@
-export const Home = () => {
-  return (
-    <div className="App">
-      <h1> Lista de presença </h1>
-    </div>
-  )
-}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+export const Home = () => {
+  return (
+    <>
+      <h1> Lista de presença </h1>
+      <input type="text" placeholder="Digite seu nome" />
+      <button> Adicionar </button>
+    </>
+  )
+}
