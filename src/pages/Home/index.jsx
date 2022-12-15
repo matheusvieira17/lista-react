568f76d5b061d00f43139c707d388effb64043c7
Matheus Vieira Ferreira <matheusvferreira17@gmail.com>
2022-12-15 09:12:58 -0300

    Working with properties inside the components

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import { Card } from "../../components/Card"
+import { Card } from "../../components/Card";
 
 export const Home = () => {
   return (
@@ -6,7 +6,9 @@ export const Home = () => {
       <h1> Lista de presença </h1>
       <input type="text" placeholder="Digite seu nome" />
       <button> Adicionar </button>
-      <Card/>
+      <Card name="Matheus" time="09:05:34" />
+      <Card name="João" time="11:15:34" />
+      <Card name="Maria" time="11:25:24" />
     </div>
-  )
-}
+  );
+};
